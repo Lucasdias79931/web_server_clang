bd63c1cd79408765e99e7282f8fcf9289e06ca08
Lucasdias79931 <lucasdiassantos888@gmail.com>
2026-04-21 18:22:52 -0300

    api feita

--- src/server.c
+++ src/server.c
@@ -3,16 +3,16 @@
 #include "sock.h"
 #include "http.h"
 #include "database.h"
-
-
-
-
+#include "string.h"
+#include <stdbool.h>
 
 int main() {
     struct Target_sock target = set_target(8000, "127.0.0.1");
     int server_fd = create_server_socket(&target);
 
-    while (1) {
+    char *database_path = "users.txt";
+
+    while (true) {
         int client_fd = accept(server_fd, NULL, NULL);
 
         char buffer[1024] = {0};
@@ -21,14 +21,70 @@ int main() {
         struct HttpRequest req;
         http_parse_request(buffer, &req);
 
-        printf("Method: %s\nPath: %s\nVersion: %s\n",
-               req.method, req.path, req.version);
-
         char response[1024];
-        http_build_response(response, 200, "text/plain", "Hello World");
+
+        if (strcmp(req.method, "POST") == 0 &&
+            strcmp(req.path, "/user") == 0) {
+
+            char *body = strstr(buffer, "\r\n\r\n");
+            if (!body) {
+                close(client_fd);
+                continue;
+            }
+            body += 4;
+
+            body[strcspn(body, "\r\n")] = 0;
+
+            struct Person_dto *person = insert_name(body, database_path);
+
+            sprintf(response,
+                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n"
+                "%s",
+                (person && person->id) ? person->id : "error"
+            );
+        }
+
+        else if (strcmp(req.method, "GET") == 0 &&
+                 strstr(req.path, "/user") != NULL) {
+
+            char id_str[64] = {0};
+
+            char *id_pos = strstr(req.path, "id=");
+            if (!id_pos) {
+                sprintf(response,
+                    "HTTP/1.1 400 Bad Request\r\n\r\nmissing id"
+                );
+                write(client_fd, response, strlen(response));
+                close(client_fd);
+                continue;
+            }
+
+            strncpy(id_str, id_pos + 3, sizeof(id_str) - 1);
+            id_str[sizeof(id_str) - 1] = 0;
+            id_str[strcspn(id_str, "& \r\n")] = 0;
+
+            struct Person_dto *person = get_name(id_str, database_path);
+
+            if (person && person->status && person->name) {
+                sprintf(response,
+                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n"
+                    "%s",
+                    person->name
+                );
+            } else {
+                sprintf(response,
+                    "HTTP/1.1 404 Not Found\r\n\r\nnot found"
+                );
+            }
+        }
+
+        else {
+            sprintf(response,
+                "HTTP/1.1 404 Not Found\r\n\r\nroute not found"
+            );
+        }
 
         write(client_fd, response, strlen(response));
-
         close(client_fd);
     }
 
